ff37cd592a7e610e69be42094bf766ee4dc55ac7
vitalspa <vitalspaterapias@gmail.com>
2025-05-07 13:31:05 -0300

    Correção: Ajuste na rota do painel de vendas e melhoria na consulta de CNPJ

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 
 // Rota para o painel de vendas
 app.get('/paineldevendas', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'fichapainel/index.html'));
 });
 
 // Rota para consulta de CNPJ (POST)
